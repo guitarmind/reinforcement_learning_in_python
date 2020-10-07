3e13d0ba1147f9610effb1c418d7963b381266f9
Mark Peng <markpeng@exosite.com>
2020-10-08 01:11:24 +0800

    Implemented Thompson-Sampling with Bayesian.

--- Bayesian-Thompson-Sampling/bayesian_starter.py
+++ Bayesian-Thompson-Sampling/bayesian_starter.py
@@ -21,19 +21,20 @@ BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
 class Bandit:
   def __init__(self, p):
     self.p = p
-    self.a = # TODO
-    self.b = # TODO
+    self.a = 1
+    self.b = 1
     self.N = 0 # for information only
 
   def pull(self):
     return np.random.random() < self.p
 
   def sample(self):
-    return # TODO - draw a sample from Beta(a, b)
+    # Draw a sample from Beta(a, b)
+    return beta.rvs(self.a, self.b, size=1)[0]
 
   def update(self, x):
-    self.a = # TODO
-    self.b = # TODO
+    self.a = self.a + x
+    self.b = self.b + 1 - x # N=1
     self.N += 1
 
 
@@ -44,7 +45,7 @@ def plot(bandits, trial):
     plt.plot(x, y, label=f"real p: {b.p:.4f}, win rate = {b.a - 1}/{b.N}")
   plt.title(f"Bandit distributions after {trial} trials")
   plt.legend()
-  plt.savefig(f"{sys.argv[0]}.png")
+  plt.savefig(f"{sys.argv[0]}_{trial}.png")
   plt.show()
 
 
@@ -55,7 +56,7 @@ def experiment():
   rewards = np.zeros(NUM_TRIALS)
   for i in range(NUM_TRIALS):
     # Thompson sampling
-    j = # TODO
+    j = np.argmax([b.sample() for b in bandits], axis=0)
 
     # plot the posteriors
     if i in sample_points:
